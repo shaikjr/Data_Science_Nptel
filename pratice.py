805fa1a8c2132b808d6b5a48d3a879499dfb1566
shaik mahaboob <111783074+shaikjr@users.noreply.github.com>
2024-07-27 22:18:24 +0530

    Add files via upload

---
+++ pratice.py
@@ -0,0 +1,19 @@
+a="gOOd moRning"
+x=a.title()
+print(x)
+
+Product = ['Pencil', 'Pen', 'Eraser', 'Pencil Box', 'Scale']
+Price= [5, 10, 2, 20, 12]  
+Brand = ['Camlin', 'Rotomac', 'Nataraj', 'Camel', 'Apsara']
+Stationery = [Product, Price, Brand]
+
+Stationery[0].insert(0,'Notebook')
+print(Stationery)
+
+set1={1,2,23,3,}
+set1.clear()
+print(set1)
+
+Mylist =['a', 'a', 'b', 'b', 'b', 'c', 'c', 'd', 'e'] 
+print(Mylist.index('d'))
+ 
